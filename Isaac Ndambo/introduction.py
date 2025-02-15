dd8dfc5490fe29571dadc97695727719c43c1d43
Ndambo254 <ndamboisaac79@gmail.com>
2025-02-15 02:41:05 -0800

    Add files via upload

---
+++ Isaac Ndambo/introduction.py
@@ -0,0 +1,32 @@
+# print("This is Python Programming")
+# print('This is python')
+# country="Kenya"
+# population="45000000"
+# print(country)
+# print(population)
+
+# print("Enter your country")
+# country=input()
+# print("What's your country's population")
+# population=input()
+# print(country)
+# print(population)
+
+# fullName="James Kamau"
+# greeting="Hello "
+# print(greeting+fullName)
+
+# fullName="James Kamau "
+# age=23
+# print(fullName+str(age))
+
+# greeting="Hello "
+# fullName="Joseph,"
+# age="you are 20 years old"
+# print(greeting+fullName+age)
+
+print("Enter your Name")
+name=input()
+print("Enter your age ")
+age=input()
+print("Hello "+name+"you are "+str(age)+"years old")
---
+++ Isaac Ndambo/mytype.py
@@ -0,0 +1,8 @@
+name="John Kamau"
+age=23
+height=5.5
+ismarried=True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(ismarried))
